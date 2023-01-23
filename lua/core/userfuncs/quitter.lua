6787570975eba510dcbb480f4f966dffdb4e733e
adishourya <shourya.aditya10@gmail.com>
2023-01-23 22:12:35 +0530

    Squash this later

--- lua/core/userfuncs/quitter.lua
+++ lua/core/userfuncs/quitter.lua
@@ -47,6 +47,7 @@ local finders = require("telescope.finders")
 local sorters = require("telescope.sorters")
 local actions = require("telescope.actions")
 local actions_state = require("telescope.actions.state")
+local conf = require("telescope.config").values
 
 local enter = function(prompt_bufnr)
 	local selected = actions_state.get_selected_entry()
@@ -61,7 +62,7 @@ end
 
 local opts = {
 	finder = finders.new_table(closeOptions),
-	sorter = sorters.get_generic_fuzzy_sorter({}),
+	sorter = conf.generic_sorter(),
 	sorting_stratergy = "ascending",
 	prompt_title = "Frequent Buffer Cmds",
 
